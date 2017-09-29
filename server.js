cc3956d1ea2cda68506b1d2c77d2e0674b87fcbd
jpeart <jpeart4@gmail.com>
2017-09-28 19:59:31 -0500

    ?

--- server.js
+++ server.js
@@ -171,9 +171,6 @@ app.post("/delete/:id", function(req, res) {
     });
 });
 
-
-
-
 // Listen on port 3000
 app.listen(process.env.PORT || 3000, function() {
     console.log("App running on port 3000!");
